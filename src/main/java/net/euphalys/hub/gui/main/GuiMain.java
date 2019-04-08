2af1badfb8bf935f7561a1cf241d9b108f09653e
LudaWeb01 <computersluda@gmail.com>
2019-04-08 16:13:49 +0200

    adding buildtest item

--- src/main/java/net/euphalys/hub/gui/main/GuiMain.java
+++ src/main/java/net/euphalys/hub/gui/main/GuiMain.java
@@ -4,6 +4,7 @@ import net.euphalys.core.api.EuphalysApi;
 import net.euphalys.hub.Hub;
 import net.euphalys.hub.gui.AbstractGui;
 import net.euphalys.hub.gui.main.game.GuiShatteredSpace;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -24,6 +25,7 @@ public class GuiMain extends AbstractGui {
     public void display(Player player) {
         this.inventory = this.hub.getServer().createInventory(null, 9 * 6, "Menu Principal");
         this.setSlotData("Build", Material.GRASS, 12, new String[]{}, "build");
+        this.setSlotData(ChatColor.DARK_GREEN + "Accès au serveurs de tests (Build)", Material.ENDER_PEARL, 13, new String[]{}, "buildtest");
         this.setSlotData(ChatColor.GOLD + "Changer de hub", Material.ENDER_CHEST, 35, makeButtonLore(new String[]{"Cliquez pour ouvrir l'interface"}, true, false), "switch_hub");
         if (EuphalysApi.getInstance().getPlayer(player.getUniqueId()).getGroup().getGroupId() >= 50)
             this.setSlotData(ChatColor.BLUE + "Shattered Space", Material.DIAMOND_SWORD, 10, makeButtonLore(new String[]{"", "", "Développeur: Dinnerwolph, trollgun", "GameDesign : FlameOfChange", "Version Minecraft : §b1.9 - 1.13 ", "§cJeu en test interne."}, true, false), "shattered");
@@ -37,6 +39,8 @@ public class GuiMain extends AbstractGui {
             this.hub.getGuiManager().openGui(player, new GuiSwitchHub(this.hub, 1));
         else if (action.equals("build"))
             EuphalysApi.getInstance().getPlayer(player.getUniqueId()).sendToServer("buildcore");
+        else if (action.equals("buildtest"))
+            EuphalysApi.getInstance().getPlayer(player.getUniqueId()).sendToServer("buildtest");
         else if (action.equals("back"))
             player.closeInventory();
         else if (action.equals("shattered"))
