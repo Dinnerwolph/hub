d76cb15454396549c7d2776bc9cc84a612e31c1c
Dinnerwolph <samu.loubiat@gmail.com>
2019-04-27 02:04:38 +0200

    fix glass state for 1.14

--- src/main/java/net/euphalys/hub/gui/PageGui.java
+++ src/main/java/net/euphalys/hub/gui/PageGui.java
@@ -1,11 +1,14 @@
-package net.euphalys.hub.gui.main;
+package net.euphalys.hub.gui;
 
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
 import fr.dinnerwolph.otl.bukkit.BukkitOTL;
 import fr.dinnerwolph.otl.bukkit.server.Server;
 import net.euphalys.api.player.IGroup;
 import net.euphalys.core.api.EuphalysApi;
 import net.euphalys.hub.Hub;
-import net.euphalys.hub.gui.AbstractGui;
+import net.euphalys.hub.gui.main.GuiMain;
+import net.euphalys.hub.gui.main.game.GuiShatteredSpace;
 import org.bukkit.ChatColor;
 import org.bukkit.DyeColor;
 import org.bukkit.Material;
@@ -13,49 +16,34 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import us.myles.ViaVersion.api.Via;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author Dinnerwolph
  */
+public abstract class PageGui extends AbstractGui {
 
-public class GuiSwitchHub extends AbstractGui {
+    protected final int page;
+    private final String template;
 
-    private final int page;
-    private final int version[] = {47, 110, 340, 477};
-    private final String server[] = {"Hub1-8", "Hub1-9", "Hub1-12", "Hub1-14"};
-
-    GuiSwitchHub(Hub hub, int page) {
+    public PageGui(Hub hub, int page, String template) {
         super(hub);
         this.page = page;
-    }
-
-    @Override
-    public void display(Player player) {
-        this.inventory = this.hub.getServer().createInventory(null, 54, "Changez de hub (Page " + this.page + ")");
-        this.update(player);
-        player.openInventory(this.inventory);
+        this.template = template;
     }
 
     @Override
     public void update(Player player) {
-        int playerVer = Via.getAPI().getPlayerVersion(player);
-        int count = -1;
-        for (int i : version)
-            if (playerVer >= i)
-                count++;
         List<Server> servers = new ArrayList();
-        for (String s : BukkitOTL.getInstance().serverList.keySet())
-            for (int i = 0; i < count + 1; i++)
-                if (BukkitOTL.getInstance().serverList.get(s).getServerName().startsWith(server[i]))
-                    servers.add(BukkitOTL.getInstance().serverList.get(s));
 
-
+        BukkitOTL.getInstance().serverList.forEach((servername, server) -> {
+            if (servername.startsWith(template))
+                if (server.getStatus().equals("WAITING"))
+                    servers.add(server);
+        });
         int[] baseSlots = {10, 11, 12, 13, 14, 15, 16};
         int line = 0, slot = 0, i = 0;
         this.inventory.clear();
@@ -89,27 +77,26 @@ public class GuiSwitchHub extends AbstractGui {
 
     @Override
     public void onClick(Player player, ItemStack stack, String action, ClickType clickType) {
-        if (action.startsWith("Hub")) {
-            if (action.equals(EuphalysApi.getInstance().getSProperty("name"))) {
-                player.sendMessage(ChatColor.RED + "Vous ne pouvez pas aller sur votre hub actuel !");
-                return;
-            }
+        if (action.startsWith(template)) {
             if (BukkitOTL.getInstance().serverList.get(action).getOnlineAmount() >= BukkitOTL.getInstance().serverList.get(action).getMaxAmount()) {
                 player.sendMessage(ChatColor.RED + "Ce Hub est plein, vous ne pouvez pas y aller.");
                 return;
             }
-            EuphalysApi.getInstance().getPlayer(player.getUniqueId()).sendToServer(action);
+            ByteArrayDataOutput out = ByteStreams.newDataOutput();
+            out.writeUTF("Connect");
+            out.writeUTF(action);
+            player.sendPluginMessage(this.hub, "BungeeCord", out.toByteArray());
         } else if (action.equals("page_back"))
-            this.hub.getGuiManager().openGui(player, new GuiSwitchHub(this.hub, (this.page - 1)));
+            this.hub.getGuiManager().openGui(player, new GuiShatteredSpace(this.hub, (this.page - 1)));
         else if (action.equals("page_next"))
-            this.hub.getGuiManager().openGui(player, new GuiSwitchHub(this.hub, (this.page + 1)));
+            this.hub.getGuiManager().openGui(player, new GuiShatteredSpace(this.hub, (this.page + 1)));
         else if (action.equals("back"))
             this.hub.getGuiManager().openGui(player, new GuiMain(this.hub));
     }
 
     private ItemStack getHubItem(Server hub) {
         ItemStack glass;
-        String baseName = "Hub " + hub.getServerName().replace("Hub", "") + " (" + hub.getOnlineAmount() + " joueur" + (hub.getOnlineAmount() > 1 ? "s" : "") + ")";
+        String baseName = "ShatteredSpace " + hub.getServerName().replace(template, "") + " (" + hub.getOnlineAmount() + " joueur" + (hub.getOnlineAmount() > 1 ? "s" : "") + ")";
         if (hub.getOnlineAmount() <= 15)
             glass = getGlass(net.euphalys.api.utils.Material.LIME_STAINED_GLASS.getBukkitMaterial(), DyeColor.GREEN, "§a" + baseName);
         else if (hub.getOnlineAmount() <= 30)
@@ -121,16 +108,12 @@ public class GuiSwitchHub extends AbstractGui {
         ItemMeta meta = glass.getItemMeta();
         List<String> lore = new ArrayList();
         Map<String, String> groups = BukkitOTL.getInstance().hubgroup.get(hub.getServerName());
-        Map<Integer, IGroup> groupMap = new ConcurrentHashMap<>();
         try {
-            groups.keySet().forEach((server) -> {
-                IGroup group = EuphalysApi.getInstance().getGroup(Integer.parseInt(server));
-                groupMap.put(group.getGroupId(), group);
-            });
-            for (int i = 100; i > 0; i--)
-                if (groupMap.get(i) != null)
-                    lore.add(groupMap.get(i).getScore() + " §r: " + groups.get(String.valueOf(i)));
-        } catch (NullPointerException e) {
+            for (String s : groups.keySet()) {
+                IGroup group = EuphalysApi.getInstance().getGroup(Integer.parseInt(s));
+                lore.add(group.getName() + " §r: " + groups.get(s));
+            }
+        } catch (Exception e) {
 
         }
 
--- src/main/java/net/euphalys/hub/gui/main/game/GuiShatteredSpace.java
+++ src/main/java/net/euphalys/hub/gui/main/game/GuiShatteredSpace.java
@@ -1,139 +1,23 @@
 package net.euphalys.hub.gui.main.game;
 
-import com.google.common.io.ByteArrayDataOutput;
-import com.google.common.io.ByteStreams;
-import fr.dinnerwolph.otl.bukkit.BukkitOTL;
-import fr.dinnerwolph.otl.bukkit.server.Server;
-import net.euphalys.api.player.IGroup;
-import net.euphalys.core.api.EuphalysApi;
 import net.euphalys.hub.Hub;
-import net.euphalys.hub.gui.AbstractGui;
-import net.euphalys.hub.gui.main.GuiMain;
-import org.bukkit.ChatColor;
-import org.bukkit.DyeColor;
-import org.bukkit.Material;
+import net.euphalys.hub.gui.PageGui;
 import org.bukkit.entity.Player;
-import org.bukkit.event.inventory.ClickType;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 /**
  * @author Dinnerwolph
  */
 
-public class GuiShatteredSpace extends AbstractGui {
-
-    private final int page;
-    private final String template = "shsp_preview";
+public class GuiShatteredSpace extends PageGui {
 
     public GuiShatteredSpace(Hub hub, int page) {
-        super(hub);
-        this.page = page;
+        super(hub, page, "shsp_preview");
     }
 
     @Override
     public void display(Player player) {
-        this.inventory = this.hub.getServer().createInventory(null, 54, "Changez de hub (Page " + this.page + ")");
+        this.inventory = this.hub.getServer().createInventory(null, 54, "Choisir un serveur (Page " + this.page + ")");
         this.update(player);
-
         player.openInventory(this.inventory);
-    }
-
-    @Override
-    public void update(Player player) {
-        List<Server> servers = new ArrayList();
-
-        BukkitOTL.getInstance().serverList.forEach((servername, server) -> {
-            if (servername.startsWith(template))
-                if (server.getStatus().equals("WAITING"))
-                    servers.add(server);
-        });
-        int[] baseSlots = {10, 11, 12, 13, 14, 15, 16};
-        int line = 0, slot = 0, i = 0;
-        this.inventory.clear();
-        this.setBackSlot();
-        boolean more = false;
-        for (Server server : servers) {
-            if (i < (7 * (this.page - 1))) {
-                i++;
-                continue;
-            } else if (i > ((7 * (this.page - 1) + (baseSlots.length * 3)))) {
-                more = true;
-                break;
-            }
-
-            this.setSlotData(this.getHubItem(server), baseSlots[slot] + (9 * line), server.getServerName());
-            slot++;
-
-            if (slot == baseSlots.length) {
-                slot = 0;
-                line++;
-            }
-            i++;
-        }
-
-        if (this.page > 1)
-            this.setSlotData(ChatColor.YELLOW + "« Page " + (this.page - 1), Material.PAPER, this.inventory.getSize() - 9, null, "page_back");
-
-        if (more)
-            this.setSlotData(ChatColor.YELLOW + "Page " + (this.page + 1) + " »", Material.PAPER, this.inventory.getSize() - 1, null, "page_next");
-    }
-
-    @Override
-    public void onClick(Player player, ItemStack stack, String action, ClickType clickType) {
-        if (action.startsWith(template)) {
-            if (BukkitOTL.getInstance().serverList.get(action).getOnlineAmount() >= BukkitOTL.getInstance().serverList.get(action).getMaxAmount()) {
-                player.sendMessage(ChatColor.RED + "Ce Hub est plein, vous ne pouvez pas y aller.");
-                return;
-            }
-            ByteArrayDataOutput out = ByteStreams.newDataOutput();
-            out.writeUTF("Connect");
-            out.writeUTF(action);
-            player.sendPluginMessage(this.hub, "BungeeCord", out.toByteArray());
-        } else if (action.equals("page_back"))
-            this.hub.getGuiManager().openGui(player, new GuiShatteredSpace(this.hub, (this.page - 1)));
-        else if (action.equals("page_next"))
-            this.hub.getGuiManager().openGui(player, new GuiShatteredSpace(this.hub, (this.page + 1)));
-        else if (action.equals("back"))
-            this.hub.getGuiManager().openGui(player, new GuiMain(this.hub));
-    }
-
-    private ItemStack getHubItem(Server hub) {
-        ItemStack glass = new ItemStack(net.euphalys.api.utils.Material.WHITE_STAINED_GLASS.getBukkitMaterial(), 1);
-        ItemMeta meta = glass.getItemMeta();
-        String baseName = "ShatteredSpace " + hub.getServerName().replace(template, "") + " (" + hub.getOnlineAmount() + " joueur" + (hub.getOnlineAmount() > 1 ? "s" : "") + ")";
-        if (hub.getOnlineAmount() <= 15) {
-            glass.setDurability(DyeColor.GREEN.getWoolData());
-            meta.setDisplayName(ChatColor.GREEN + baseName);
-        } else if (hub.getOnlineAmount() <= 30) {
-            glass.setDurability(DyeColor.YELLOW.getWoolData());
-            meta.setDisplayName(ChatColor.YELLOW + baseName);
-        } else if (hub.getOnlineAmount() <= 40) {
-            glass.setDurability(DyeColor.RED.getWoolData());
-            meta.setDisplayName(ChatColor.RED + baseName);
-        } else {
-            glass.setDurability(DyeColor.YELLOW.getWoolData());
-            meta.setDisplayName(ChatColor.YELLOW + baseName);
-        }
-
-        List<String> lore = new ArrayList();
-        Map<String, String> groups = BukkitOTL.getInstance().hubgroup.get(hub.getServerName());
-        try {
-            for (String s : groups.keySet()) {
-                IGroup group = EuphalysApi.getInstance().getGroup(Integer.parseInt(s));
-                lore.add(group.getName() + " §r: " + groups.get(s));
-            }
-        } catch (Exception e) {
-
-        }
-
-        lore.add(ChatColor.DARK_GRAY + "▶ Cliquez pour être téléporté");
-        meta.setLore(lore);
-        glass.setItemMeta(meta);
-        return glass;
     }
 }
